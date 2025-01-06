59f005606dac7618297d3190ab6e94157be1deab
SannidhyaMaheshwari <sannidhyamaheshwari1328@gmail.com>
2025-01-06 23:07:09 +0530

    creation and intial data

---
+++ sq1/creation.sql
@@ -0,0 +1,60 @@
+create database BITS;
+
+CREATE TABLE MASTER_T (
+  app_no VARCHAR(20) PRIMARY KEY,
+  name VARCHAR(20) NOT NULL
+);
+
+
+CREATE TABLE ITERATION_OFFER (
+  app_no VARCHAR(20) PRIMARY KEY,
+  itr1_offer VARCHAR(20) NOT NULL,
+  itr1_scholarship int,
+  itr1_uploaded_by VARCHAR(20) NOT NULL,
+  itr1_upload_date_time datetime,
+  itr2_offer VARCHAR(20) NOT NULL,
+  itr2_scholarship int,
+  itr2_uploaded_by VARCHAR(20) NOT NULL,
+  itr2_upload_date_time datetime,
+  itr3_offer VARCHAR(20) NOT NULL,
+  itr3_scholarship int,
+  itr3_uploaded_by VARCHAR(20) NOT NULL,
+  itr3_upload_date_time datetime,
+  itr4_offer VARCHAR(20) NOT NULL,
+  itr4_scholarship int,
+  itr4_uploaded_by VARCHAR(20) NOT NULL,
+  itr4_upload_date_time datetime,
+  itr5_offer VARCHAR(20) NOT NULL,
+  itr5_scholarship int,
+  itr5_uploaded_by VARCHAR(20) NOT NULL,
+  itr5_upload_date_time datetime,
+  itr6_offer VARCHAR(20) NOT NULL,
+  itr6_scholarship int,
+  itr6_uploaded_by VARCHAR(20) NOT NULL,
+  itr6_upload_date_time datetime
+);
+
+
+CREATE TABLE ITERATION_DATE (
+  iteration int PRIMARY KEY,
+  date datetime NOT NULL
+);
+
+CREATE TABLE FEES_PAID (
+  app_no VARCHAR(20) PRIMARY KEY,
+  fees_paid_date datetime NOT NULL,
+  uploaded_by VARCHAR(20) NOT NULL,
+  upload_date_time datetime
+);
+
+CREATE TABLE WITHDRAWS (
+  app_no VARCHAR(20) PRIMARY KEY,
+  date datetime NOT NULL,
+  uploaded_by VARCHAR(20) NOT NULL,
+  upload_date_time datetime
+);
+
+
+
+
+
